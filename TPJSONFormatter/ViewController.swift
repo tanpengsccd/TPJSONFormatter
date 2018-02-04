6b855f623f76542e936215c49658d8b42f44578c
TanPeng <tanpengcd@gmail.com>
2018-02-04 19:22:12 +0800

    Initial Commit

---
+++ TPJSONFormatter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TPJSONFormatter
+//
+//  Created by 谭鹏 on 2018/2/4.
+//  Copyright © 2018年 谭鹏. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
