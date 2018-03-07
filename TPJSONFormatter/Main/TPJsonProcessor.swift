56e736bb937215581463e54cb722d4885220fd55
TanPeng <tanpengcd@gmail.com>
2018-03-08 00:20:31 +0800

    修复 0 被识别为 bool 的问题

--- TPJSONFormatter/Main/TPJsonProcessor.swift
+++ TPJSONFormatter/Main/TPJsonProcessor.swift
@@ -57,15 +57,15 @@ class TPJsonProcessor {
                 
             }
             return TPJsonModel.arrays(lastJsonValue)
-        }else if let _ = value as? Bool{
-                return TPJsonModel.simpleValue(.bool)
-            
         }else if let double = value as? Double {
             if floor(double) == double {
                 return TPJsonModel.simpleValue(.int)
             }else{
                 return TPJsonModel.simpleValue(.double)
             }
+        }else if let _ = value as? Bool{
+                return TPJsonModel.simpleValue(.bool)
+            
         }else if let string = value as? String{
             //还可以再处理 bool ， int ， double
             if ["true","false"] .contains(string){
@@ -85,6 +85,52 @@ class TPJsonProcessor {
             return nil
         }
     }
+}
+
+class Root{
+    class Servicepool{
+        var f_name :String?
+        var f_code :String?
+    }
+    
+    var servicePool :[Servicepool]?
+    class Fundpool{
+        var f_name :String?
+        var f_code :String?
+    }
+    
+    var fundPool :[Fundpool]?
+    var error :Int?
+    class Datas{
+        var f_org_id :Int?
+        var f_usergroup_id :Int?
+        var f_password :String?
+        var f_user_code :Int?
+        var f_birth_date :String?
+        var f_user_id :Int?
+        var f_rank :Int?
+        var f_mobile :String?
+        var f_real_name :String?
+        var f_salt :String?
+        var f_validation_type :String?
+        var f_validation_key :String?
+        var f_theme :String?
+        var f_gender :String?
+        var f_type :Int?
+        var f_email :String?
+        var f_image :String?
+        var f_username :String?
+        var f_status :Int?
+    }
+    
+    var datas :[Datas]?
+    class Material{
+        var f_name :String?
+        var f_code :String?
+    }
+    
+    var material :[Material]?
+    var errormsg :String?
 }
 
 
